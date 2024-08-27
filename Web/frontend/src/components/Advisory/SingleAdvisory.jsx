136bfa7984b35f799f4daa03a88d2d659756d7e7
Harsh <harshypimparkar@gmail.com>
2024-08-27 20:21:52 +0530

    Alert Updated

--- Web/frontend/src/components/Advisory/SingleAdvisory.jsx
+++ Web/frontend/src/components/Advisory/SingleAdvisory.jsx
@@ -3,24 +3,30 @@ import { GoAlert } from "react-icons/go";
 
 const SingleAdvisory = ({ item, index }) => {
   return (
-    <div className="single-card">
-      <div className="icon-container">
-        <GoAlert size={25} />
-      </div>
+    <div className="">
+      {" "}
+      {/* Add class to wrap advisory */}
       <div className="advisory-card">
-        <h3 className="advisory-title">{item.title?.slice(0, 5)}</h3>
+        <div className="icon-container">
+          <GoAlert size={40} className="advisory-icon" />{" "}
+          {/* Corrected className */}
+        </div>
+        <h3 className="advisory-title">{item.title?.slice(0, 20)}</h3>{" "}
+        {/* Increased slice length for better display */}
         <div className="advisory-details">
           <p className="advisory-description">
-            {item.description?.slice(0, 5)}
+            {item.description?.slice(0, 100)}{" "}
+            {/* Increased slice length for better display */}
           </p>
-          <div className="advisory-date-time">
+          <div className="advisory-time">
             <div>Date: {item.date}</div>
             <div>Time: {item.time}</div>
           </div>
-          <p className="advisory-location">
-            <div>City: {item.cityName?.slice(0, 5)}</div>
+          <div className="advisory-city">
+            <div>City: {item.cityName?.slice(0, 20)}</div>{" "}
+            {/* Increased slice length for better display */}
             <div>Pincode: {item.pincode?.slice(0, 6)}</div>
-          </p>
+          </div>
         </div>
       </div>
     </div>
